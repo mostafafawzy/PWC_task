96915ffae037cebd52db4a36ead9a3f4357d134b
Mostafa Salem <mostafa.salem3@vodafone.com.eg>
2025-09-01 21:37:27 +0300

    update_pipeline

--- app/main.py
+++ app/main.py
@@ -2,5 +2,8 @@ from app import create_app
 
 app = create_app()
 
+@app.route("/")
+def home():
+    return "Hello from Flask root route!"
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
