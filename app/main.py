98cb9ad1856ea3b631bd2ba11d538761e51b056c
Mostafa Salem <mostafa.salem3@vodafone.com.eg>
2025-09-03 14:43:17 +0300

    update_main

--- app/main.py
+++ app/main.py
@@ -1,19 +1,13 @@
-    from app import create_app
-    from prometheus_flask_exporter import PrometheusMetrics
+from app import create_app
+
+app = create_app()
 
 
-    app = create_app()
-    metrics = PrometheusMetrics(app)
 
-    # Custom metric
-    endpoint_counter = metrics.counter(
-        'custom_users_hits', 'Number of hits to /users endpoint'
-    )
+@app.route("/")
+def home():
+    return "Hello from Flask root route!"
+
     
-    @app.route("/")
-    def home():
-        return "Hello from Flask root route!"
-
-        
-    if __name__ == '__main__':
-        app.run(host='0.0.0.0', port=5000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
